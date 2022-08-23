24279dcb189a85933d3d9cd830a695521f80ec70
mahimagupta24 <mahimagupta2401@gmail.com>
2022-08-23 23:47:52 +0530

    added spaces after questions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var readlineSync = require("readline-sync");
-var userName = readlineSync.question("what is your name?");
+var userName = readlineSync.question("what is your name? ");
 var score = 0;
 console.log("welcome " + userName + " to how well do you know Mahima?");
 //play function
@@ -17,23 +17,23 @@ function play(question, answer) {
 
 var questions = [
   {
-    question: "Where do i live?",
+    question: "Where do i live? ",
     answer: "udhampur",
   },
   {
-    question: "Which is my favourite color?",
+    question: "Which is my favourite color? ",
     answer: "black",
   },
   {
-    question: "My favourite sport would be?",
+    question: "My favourite sport would be? ",
     answer: "cricket",
   },
   {
-    question: "Who is my favouirte player ? ",
+    question: "Who is my favouirte player? ",
     answer: "virat kohli",
   },
   {
-    question: "My favourite food is?",
+    question: "My favourite food is? ",
     answer: "momos",
   },
 ];
